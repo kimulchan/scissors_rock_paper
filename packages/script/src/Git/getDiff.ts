f46b627b5e022585fdddb049028c796c05346e2e
kimuichan <leerabi577@gmail.com>
2023-05-02 16:16:05 +0900

    gogogo

--- packages/script/src/Git/getDiff.ts
+++ packages/script/src/Git/getDiff.ts
@@ -1,9 +1,9 @@
-import execution from '../Shell/execution';
-import splitSpacer from '../Utils/splitSpacer';
+import execution from "../Shell/execution";
+import splitSpacer from "../Utils/splitSpacer";
 
 async function getDiff() {
-    const { stdout } = await execution('git diff --name-only');
-    return splitSpacer(stdout);
+  const { stdout } = await execution("git diff HEAD HEAD^ --name-only");
+  return splitSpacer(stdout);
 }
 
 export default getDiff;
